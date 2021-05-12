6588cbfea96ee6704a4c014ca72d04ea4b0d2fe0
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2021-05-12 16:36:43 +0530

    [vishnu] removed an unwanted PATH setting

--- start-qemu.sh
+++ start-qemu.sh
@@ -4,8 +4,6 @@ BR2_DIR="${PWD}/buildroot-2014.08"
 OUT_DIR="${BR2_DIR}/output/images"
 EXTRA_ARGS="-nographic -m 128"
 
-export PATH="/home/easwaran/ajit_proc/git-ajit-toolchain/os/Ajit_linux/build-qemu/buildroot-2014.08/output/host/usr/bin:${PATH}"
-
 exec qemu-system-sparc \
     -M SS-10 \
     -kernel ${OUT_DIR}/zImage \
