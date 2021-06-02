1bed363ddff3d38a20c3d18de48adc50e2c10b45
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2021-06-02 11:15:37 +0530

    [vishnu] - (build.sh) modified script to create a build log

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+TOP_DIR="${PWD}"
 WORKSPACE_DIR="${PWD}/workspace"
 BR2_DIR="${PWD}/buildroot-2014.08"
 OUT_DIR="${BR2_DIR}/output/images"
 
 cd $BR2_DIR
 
-make clean
-make BR2_EXTERNAL=${WORKSPACE_DIR}/qemu_sparc_ss10_configs qemu_sparc_ss10_rootfs_defconfig
-make && \
-cp ${OUT_DIR}/rootfs.tar ${WORKSPACE_DIR}/overlay
+build ()
+{
+	make clean
+	make BR2_EXTERNAL=${WORKSPACE_DIR}/qemu_sparc_ss10_configs qemu_sparc_ss10_rootfs_defconfig
+	make && \
+	cp ${OUT_DIR}/rootfs.tar ${WORKSPACE_DIR}/overlay
 
-# creating initramfs. cpio not embedded in linux
-make BR2_EXTERNAL=${WORKSPACE_DIR}/qemu_sparc_ss10_configs qemu_sparc_ss10_initramfs_defconfig
-make
+	# creating initramfs. cpio not embedded in linux
+	make BR2_EXTERNAL=${WORKSPACE_DIR}/qemu_sparc_ss10_configs qemu_sparc_ss10_initramfs_defconfig
+	make
+}
+
+build |& tee ${TOP_DIR}/build.log;
